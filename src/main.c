af872cd1cb18d5b1b29df10b50bee70d8be31c74
ArpNova <arpanbiswas.myworld@gmail.com>
2025-12-04 19:12:29 +0530

    add lsh_help and lsh_exit function implementations

--- src/main.c
+++ src/main.c
@@ -44,6 +44,22 @@ int lsh_cd(char **args){
     return 1;
 }
 
+int lsh_help(char **args){
+    printf("Arpan Biswas's SHELL\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for(int i = 0; i < lsh_num_biultins(); i++){
+        printf(" %s\n", buildin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args){
+    return 0;
+}
 
 
 int lsh_launch(char **args){
