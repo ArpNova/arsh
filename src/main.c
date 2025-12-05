ca89105feced7b9a45ef2023128b75f27eb28332
ArpNova <arpanbiswas.myworld@gmail.com>
2025-12-05 12:18:08 +0530

    feat: add prompt display in lsh_loop for better user interaction

--- src/main.c
+++ src/main.c
@@ -172,7 +172,8 @@ void lsh_loop(void){
     char **args;
     int status;
     do
-    {
+    {   
+        printf("> ");
         line = lsh_read_line();
         args = lsh_split_line(line);
         status = lsh_execute(args);
